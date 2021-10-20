4af68dfd4c7c3f832392c67fc413e6cb17576f61
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-10-20 17:30:49 +0800

    157. Rendering a Fallback Text

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { NavigationDrawerProp } from 'react-navigation-drawer';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
@@ -6,6 +7,7 @@ import { useSelector } from 'react-redux';
 
 import { RootState } from '../App';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
 import MealList from '../components/MealList';
 import Meal from '../models/meal';
 
@@ -13,6 +15,14 @@ const FavoritesScreen: NavigationStackScreenComponent = (props) => {
   const favoriteMeals = useSelector<RootState, Meal[]>(
     (state) => state.meals.favoriteMeals
   );
+
+  if (!favoriteMeals || favoriteMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 };
@@ -37,3 +47,11 @@ FavoritesScreen.navigationOptions = (navigationData) => {
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
