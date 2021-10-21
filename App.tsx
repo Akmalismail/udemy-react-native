3ead1089edb5568f89adc56b276e3ff7e63dc56c
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-10-21 18:24:31 +0800

    171. Using Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 
@@ -11,8 +13,26 @@ const rootReducer = combineReducers({
 export type RootState = ReturnType<typeof rootReducer>;
 
 const store = createStore(rootReducer);
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.error("[AppLoading]", err)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
