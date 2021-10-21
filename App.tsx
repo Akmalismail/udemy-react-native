5a0aac601fcab979d47b540974a3f5d8316f6f0c
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-10-21 11:59:41 +0800

    starting app

--- App.tsx
+++ App.tsx
@@ -1,46 +1,21 @@
-import AppLoading from 'expo-app-loading';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { enableScreens } from 'react-native-screens';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-
-import MealsNavigator from './navigation/MealsNavigator';
-import mealsReducer from './store/reducers/meals';
-
-enableScreens();
-
-const rootReducer = combineReducers({
-  meals: mealsReducer,
-});
-export type RootState = ReturnType<typeof rootReducer>;
-
-const store = createStore(rootReducer);
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  // if font not loaded yet, maintain splash screen
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={(err) => console.error("[AppLoading]", err)}
-      />
-    );
-  }
-
   return (
-    <Provider store={store}>
-      <MealsNavigator />
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
