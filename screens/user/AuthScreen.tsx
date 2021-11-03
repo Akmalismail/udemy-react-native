bfc67e458e45e9f29754d943ade5615253aaf721
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-11-03 15:03:27 +0800

    219. Adding User Signup

--- screens/user/AuthScreen.tsx
+++ screens/user/AuthScreen.tsx
@@ -1,58 +1,157 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
-import { Button, KeyboardAvoidingView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useReducer } from 'react';
+import {
+    Button, Keyboard, KeyboardAvoidingView, ScrollView, StyleSheet, TouchableWithoutFeedback, View
+} from 'react-native';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { useDispatch } from 'react-redux';
 
 import Card from '../../components/ui/Card';
 import Input from '../../components/ui/Input';
 import Colors from '../../constants/Colors';
+import * as authActions from '../../store/actions/auth';
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+type InputIdentifier = "email" | "password";
+type FormReducerAction = {
+  type: typeof FORM_INPUT_UPDATE;
+  value: string;
+  isValid: boolean;
+  input: InputIdentifier;
+};
+type FormState = {
+  inputValues: {
+    email: string;
+    password: string;
+  };
+  inputValidities: {
+    [key in InputIdentifier]: boolean;
+  };
+  formIsValid: boolean;
+};
+const formReducer = (
+  state: FormState,
+  action: FormReducerAction
+): FormState => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value,
+    };
+    const updatedValidities: { [key in InputIdentifier]: boolean } = {
+      ...state.inputValidities,
+      [action.input]: action.isValid,
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid =
+        updatedFormIsValid && updatedValidities[key as InputIdentifier];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValues: updatedValues,
+      inputValidities: updatedValidities,
+    };
+  }
+
+  return state;
+};
 
 const AuthScreen: NavigationStackScreenComponent = () => {
+  const dispatch = useDispatch();
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: "",
+      password: "",
+    },
+    inputValidities: {
+      email: false,
+      password: false,
+    },
+    formIsValid: false,
+  });
+
+  const inputChangeHandler = useCallback(
+    (
+      inputIdentifier: InputIdentifier,
+      inputValue: string,
+      inputValidity: boolean
+    ) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier,
+      });
+    },
+    [dispatchFormState]
+  );
+
+  const signUpHandler = () => {
+    dispatch(
+      authActions.signUp(
+        formState.inputValues.email,
+        formState.inputValues.password
+      )
+    );
+  };
+
   return (
     <KeyboardAvoidingView
       style={{ flex: 1, ...styles.screen }}
       behavior="padding"
       keyboardVerticalOffset={-150}
     >
-      <LinearGradient colors={["#ffedff", "#ffe3ff"]} style={styles.gradient}>
-        <Card style={styles.authContainer}>
-          <ScrollView>
-            <Input
-              id="email"
-              label="E-Mail"
-              keyboardType="email-address"
-              required
-              email
-              autoCapitalize="none"
-              errorText="Please enter a valid email address."
-              onInputChange={() => {}}
-              initialValue=""
-            />
-            <Input
-              id="password"
-              label="Password"
-              keyboardType="default"
-              secureTextEntry
-              required
-              minLength={5}
-              autoCapitalize="none"
-              errorText="Please enter a valid password."
-              onInputChange={() => {}}
-              initialValue=""
-            />
-            <View style={styles.buttonContainer}>
-              <Button title="Login" color={Colors.primary} onPress={() => {}} />
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                title="Switch to Sign Up"
-                color={Colors.accent}
-                onPress={() => {}}
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <LinearGradient colors={["#ffedff", "#ffe3ff"]} style={styles.gradient}>
+          <Card style={styles.authContainer}>
+            <ScrollView>
+              <Input
+                id="email"
+                label="E-Mail"
+                keyboardType="email-address"
+                required
+                email
+                autoCapitalize="none"
+                errorText="Please enter a valid email address."
+                onInputChange={inputChangeHandler as any}
+                initialValue=""
               />
-            </View>
-          </ScrollView>
-        </Card>
-      </LinearGradient>
+              <Input
+                id="password"
+                label="Password"
+                keyboardType="default"
+                secureTextEntry
+                required
+                minLength={5}
+                autoCapitalize="none"
+                errorText="Please enter a valid password."
+                onInputChange={inputChangeHandler as any}
+                initialValue=""
+              />
+              <View style={styles.buttonContainer}>
+                <Button
+                  title="Login"
+                  color={Colors.primary}
+                  onPress={signUpHandler}
+                />
+              </View>
+              <View style={styles.buttonContainer}>
+                <Button
+                  title="Switch to Sign Up"
+                  color={Colors.accent}
+                  onPress={() => {}}
+                />
+              </View>
+            </ScrollView>
+          </Card>
+        </LinearGradient>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 };
---
+++ store/actions/auth.tsx
@@ -0,0 +1,37 @@
+export const SIGN_UP = "SIGN_UP";
+export type SignUpAction = {
+  type: typeof SIGN_UP;
+};
+export const signUp = (email: string, password: string) => {
+  return async (dispatch: (action: SignUpAction) => void) => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyClZQBT6Eek3xcDN8IG8niywrZWVQnjDg0",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const responseData = await response.json();
+      console.log("Response", responseData);
+
+      dispatch({
+        type: SIGN_UP,
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
+};
