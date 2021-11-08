39f900b16f2f4db8567b150765613d263b16a425
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-11-08 12:51:23 +0800

    244. Diving into SQLite for Permanent Data Storage

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
+import { init } from './helpers/db';
 import PlacesNavigator from './navigation/PlacesNavigator';
 import { rootReducer } from './store/store';
+
+init().then(
+  () => {
+    console.log("Initialized database");
+  },
+  (err) => {
+    console.log("Initializing db failed");
+    console.log(err);
+  }
+);
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
--- store/places-action.tsx
+++ store/places-action.tsx
@@ -3,39 +3,6 @@ import * as FileSystem from 'expo-file-system';
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const ADD_PLACE = "ADD_PLACE";
-// export const addPlace = createAction(
-//   ADD_PLACE,
-//   (title: string, image: string) => {
-//     // const fileName = newPlace.image.split("/").pop();
-//     // const newPath = (FileSystem.documentDirectory as string) + fileName;
-
-//     // try {
-//     //   await FileSystem.moveAsync({
-//     //     from: newPlace.image,
-//     //     to: newPath,
-//     //   });
-//     // } catch (error) {
-//     //   console.log(error);
-//     //   throw error;
-//     // }
-//     // return  dispatch({
-//     //   type: ADD_PLACE,
-//     //   placeData: {
-//     //     title: newPlace.title,
-//     //     image: newPath,
-//     //   },
-//     // });
-
-//     return {
-//       payload: {
-//         placeData: {
-//           title: title,
-//           image: image,
-//         },
-//       },
-//     };
-//   }
-// );
 
 export const addPlace = createAsyncThunk(
   ADD_PLACE,
@@ -49,7 +16,6 @@ export const addPlace = createAsyncThunk(
         to: newPath,
       });
     } catch (error) {
-      console.log(error);
       throw error;
     }
 
