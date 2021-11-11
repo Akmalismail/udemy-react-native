dbbec40298037d5594528a2eefce7207d3d787a4
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2021-11-11 17:18:19 +0800

    300. Sending Local Notifications

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const triggerNotificationHandler = async () => {
+    const permissionResult = await Notifications.requestPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert(
+        "Permission required!",
+        "Please enable notification permissions",
+        [{ text: "Okay" }]
+      );
+      return;
+    }
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the first local notification  we are sending!",
+      },
+      trigger: {
+        seconds: 10,
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Trigger Notification"
+        onPress={triggerNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
